d37643963e261158377655a3f679e9517b413e11
stoian2662 <stoian2662@gmail.com>
2024-01-10 23:17:41 +0200

    Revert "IntervalSet and Interval with Value added."
    This reverts commit f4df89699344ac8318b4f36fb870c0ae801d191d.

--- EasyIntervals/Interval.cs
+++ EasyIntervals/Interval.cs
@@ -11,24 +11,10 @@ public enum IntervalType
     Closed = StartClosed | EndClosed,
 }
 
-public interface IInterval<TLimit>
-{
-    public TLimit Start { get; }
-
-    public TLimit End { get; }
-
-    public IntervalType Type { get; }
-}
-
-public interface INodeInterval<TLimit>
-{
-    public TLimit MaxEnd { get; set; }
-}
-
 /// <summary>
 /// Interval.
 /// </summary>
-public struct Interval<TLimit> : IInterval<TLimit>, INodeInterval<TLimit>, IEquatable<Interval<TLimit>>
+public struct Interval<TLimit> : IEquatable<Interval<TLimit>>
 {
     public Interval(TLimit start, TLimit end, IntervalType type = IntervalType.Open, IComparer<TLimit>? comparer = null)
     {
@@ -57,12 +43,6 @@ public struct Interval<TLimit> : IInterval<TLimit>, INodeInterval<TLimit>, IEqua
     public IntervalType Type { get; init; }
 
     internal TLimit MaxEnd { get; set; }
-
-    TLimit INodeInterval<TLimit>.MaxEnd
-    {
-        get => MaxEnd;
-        set => MaxEnd = value;
-    }
 
     public override int GetHashCode()
     {
@@ -110,95 +90,4 @@ public struct Interval<TLimit> : IInterval<TLimit>, INodeInterval<TLimit>, IEqua
 
     public static implicit operator Interval<TLimit>((TLimit Start, TLimit End, IntervalType Type, IComparer<TLimit> Comparer) interval) =>
         new Interval<TLimit>(interval.Start, interval.End, interval.Type, interval.Comparer);
-}
-
-/// <summary>
-/// Interval with value.
-/// </summary>
-public struct Interval<TLimit, TValue> : IInterval<TLimit>, INodeInterval<TLimit>, IEquatable<Interval<TLimit, TValue>>
-{
-    public Interval(TLimit start, TLimit end, TValue? value, IntervalType type = IntervalType.Open, IComparer<TLimit>? comparer = null)
-    {
-        comparer ??= Comparer<TLimit>.Default;
-        var startEndComparison = comparer.Compare(start, end);
-        if (startEndComparison > 0)
-        {
-            throw new ArgumentException("Start must not be greater than end.");
-        }
-
-        if (startEndComparison == 0 && type != IntervalType.Closed)
-        {
-            throw new ArgumentException("Equal limits must be combined only with Closed interval type.");
-        }
-
-        Start = start;
-        End = end;
-        Value = value;
-        Type = type;
-        MaxEnd = End;
-    }
-
-    public TLimit Start { get; init; }
-
-    public TLimit End { get; init; }
-
-    public TValue? Value { get; init; }
-
-    public IntervalType Type { get; init; }
-
-    internal TLimit MaxEnd { get; set; }
-
-    TLimit INodeInterval<TLimit>.MaxEnd
-    {
-        get => MaxEnd;
-        set => MaxEnd = value;
-    }
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(Start, End, Type, Value);
-    }
-
-    public override bool Equals(object? obj)
-    {
-        if (obj is Interval<TLimit, TValue> other)
-        {
-            return Equals(other);
-        }
-
-        return false;
-    }
-
-    public bool Equals(Interval<TLimit, TValue> other)
-    {
-        var result = Type.Equals(other!.Type)
-            && (Start?.Equals(other.Start) ?? other.Start is null)
-            && (End?.Equals(other.End) ?? other.End is null)
-            && (Value?.Equals(other.Value) ?? other.Value is null);
-        if (!result)
-        {
-            return false;
-        }
-
-        return result;
-    }
-
-    public override string ToString()
-    {
-        var startBracket = (Type & IntervalType.StartClosed) == IntervalType.StartClosed ? '[' : '(';
-        var endBracket = (Type & IntervalType.EndClosed) == IntervalType.EndClosed ? ']' : ')';
-        return $"{startBracket}{Start}, {End}{endBracket}";
-    }
-
-    public static implicit operator Interval<TLimit, TValue>((TLimit Start, TLimit End, TValue? Value) interval) =>
-        new Interval<TLimit, TValue>(interval.Start, interval.End, interval.Value);
-
-    public static implicit operator Interval<TLimit, TValue>((TLimit Start, TLimit End, TValue? Value, IntervalType Type) interval) =>
-        new Interval<TLimit, TValue>(interval.Start, interval.End, interval.Value, interval.Type);
-
-    public static implicit operator Interval<TLimit, TValue>((TLimit Start, TLimit End, TValue? Value, IComparer<TLimit> Comparer) interval) =>
-        new Interval<TLimit, TValue>(interval.Start, interval.End, interval.Value, IntervalType.Open, interval.Comparer);
-
-    public static implicit operator Interval<TLimit, TValue>((TLimit Start, TLimit End, TValue? Value, IntervalType Type, IComparer<TLimit> Comparer) interval) =>
-        new Interval<TLimit, TValue>(interval.Start, interval.End, interval.Value, interval.Type, interval.Comparer);
 }
